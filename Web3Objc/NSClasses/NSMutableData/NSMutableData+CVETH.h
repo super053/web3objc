4b4d3043e63fc8005401c10dc5a9b8740e1b08fc
박성식 <super053@gmail.com>
2020-05-13 15:49:45 +0900

    remove warning

--- Web3Objc/NSClasses/NSMutableData/NSMutableData+CVETH.h
+++ Web3Objc/NSClasses/NSMutableData/NSMutableData+CVETH.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
-CFAllocatorRef SecureAllocator();
+CFAllocatorRef SecureAllocator(void);
 
 @interface NSMutableData (CVETH)
 + (NSMutableData *)secureDataWithCapacity:(NSUInteger)aNumItems;
